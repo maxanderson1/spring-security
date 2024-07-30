f5cc330e2171a060e06f5d5c3d53c8da0a598540
Max Anderson <max.anderson1@ymail.com>
2024-07-30 20:27:46 +0100

    first oauth commit

---
+++ src/test/java/com/security/oauth1/Oauth1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.oauth1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
